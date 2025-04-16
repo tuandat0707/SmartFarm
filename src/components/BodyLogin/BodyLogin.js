214e4b6e2d36637a892c05d8690ef57df1b95256
Đạt Phạm <phamtuandat7704@gmail.com>
2025-04-16 21:18:28 +0700

    Initial commit

--- src/components/BodyLogin/BodyLogin.js
+++ src/components/BodyLogin/BodyLogin.js
@@ -1,26 +1,36 @@
-import React from 'react'
-import { useGlobalContext } from '../../context'
-import "./BodyLogin.scss"
+import React from 'react';
+import { useGlobalContext } from '../../context';
+import "./BodyLogin.scss";
+
 export default function BodyLogin() {
     const { setUser } = useGlobalContext();
+
     const handleSubmit = () => {
         setUser(true);
-    }
-    
-    return (
-        <div className='body-login'>
-            <div className='body-login-left'>
-                <img src='./farm.jpg' />
-            </div>
-            <div className='body-login-right'>
-                <h2>Welcome To Strawberry farm</h2>
-                <h3>Login</h3>
-                <input placeholder='Username' type="text" />
-                <input placeholder='Password' type="password" />
+    };
 
-                <button className='btn-login' onClick={handleSubmit}>Login</button>
-                <button className='btn-signup'>Goto sign up</button>
+    return (
+        <div className="body-login">
+            <div className="body-login-header">
+                <span className="leaf-icon">🍃</span>
+                <h1>Smart Farm</h1>
+            </div>
+            <div className="body-login-form">
+                <h2>Welcome back</h2>
+                <p>Please enter your credentials to login</p>
+                <input placeholder="Enter your email" type="email" />
+                <input placeholder="Enter your password" type="password" />
+                <div className="form-options">
+                    <label>
+                        <input type="checkbox" /> Remember me
+                    </label>
+                    <a href="#" className="forgot-password">Forgot password?</a>
+                </div>
+                <button className="btn-login" onClick={handleSubmit}>Login</button>
+                <p className="signup-link">
+                    Don’t have an account? <a href="#">Sign up</a>
+                </p>
             </div>
         </div>
-    )
+    );
 }
